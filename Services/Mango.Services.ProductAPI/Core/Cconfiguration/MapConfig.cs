14a2f63ccf4d1d3edf50d6dc0c0be0287fe11f70
Ersin Gun <ersngun@gmail.com>
2022-06-22 14:44:31 +0300

    AutoMapper Configuration Adeed.

---
+++ Services/Mango.Services.ProductAPI/Core/Cconfiguration/MapConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dtos;
+
+namespace Mango.Services.ProductAPI.Core.Cconfiguration
+{
+    public class MapConfig
+    {
+        public static MapperConfiguration RegisterMap()
+        {
+            var mapConfig = new MapperConfiguration(c =>
+              {
+                  c.CreateMap<ProductDto, Product>().ReverseMap();
+              });
+
+            return mapConfig;
+        }
+    }
+}
--- Services/Mango.Services.ProductAPI/Program.cs
+++ Services/Mango.Services.ProductAPI/Program.cs
@@ -1,3 +1,5 @@
+using AutoMapper;
+using Mango.Services.ProductAPI.Core.Cconfiguration;
 using Mango.Services.ProductAPI.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -8,6 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); 
+
+IMapper mapper=MapConfig.RegisterMap().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
